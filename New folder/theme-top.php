2d8654c3e90ac9594bf4269fbe18c62c1875acda
vank002 <roger.van.koughnett@gmail.com>
2012-03-23 13:03:36 -0400

    removed unused files

--- New folder/theme-top.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML>
-<html lang=en-ca>
-<head>
-	<meta charset=utf-8>
-	<title><?php if (isset($title)) { echo $title . ' · '; } ?>Dinosaurs Bones!</title>
-	<link href="css/dinobones.css" rel="stylesheet">
-	<script src="js/modernizr.dev.js"></script>
-</head>
-<body>
